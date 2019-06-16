06b6675244f0844e2f18364977b8b6716097a0cb
ramidez <1aezakmi1>
2019-06-16 19:11:46 +0200

    Konfiguracja pod tomcat application

--- src/main/java/com/ramidez/warehouse/WarehouseApplication.java
+++ src/main/java/com/ramidez/warehouse/WarehouseApplication.java
@@ -2,13 +2,23 @@ package com.ramidez.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WarehouseApplication {
+public class WarehouseApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WarehouseApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WarehouseApplication.class);
+    }
 }
 
