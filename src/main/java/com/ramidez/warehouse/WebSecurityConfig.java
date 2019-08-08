b465872ea07ab2de3d6c18f53e778a44bdefe34f
ramidez <1aezakmi1>
2019-08-08 11:12:58 +0200

    change password

--- src/main/java/com/ramidez/warehouse/WebSecurityConfig.java
+++ src/main/java/com/ramidez/warehouse/WebSecurityConfig.java
@@ -33,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
+                        .username("atmosfera")
+                        .password("DobraAtmosfera100%")
                         .roles("USER")
                         .build();
 
